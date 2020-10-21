0984985c86b93a00120e553ec72cd5b2ca68ea4a
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:39:01 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan9/src/main/java/com/mycompany/pbo_10119902_latihan9/PBO_10119902_Latihan9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan9;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char: " + tab + "berisi " + c );
+    }
+    
+}
